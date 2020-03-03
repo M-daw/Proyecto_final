6fa97192f5681bb75de80f7626f4a0c87c3fb0ae
Maribel Carreres <micarreres@gmail.com>
2020-03-03 20:37:11 +0100

    v1.0 index.html

---
+++ css/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Herbario On-line</title>
+  <meta name="description" content="HTML5">
+  <meta name="author" content="M">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="swiper/css/swiper.min.css">
+  <!-- librerías -->
+  <script src="jquery-3.4.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="swiper/js/swiper.min.js"></script>
+
+  <script src="scripts.js"></script>
+
+</head>
